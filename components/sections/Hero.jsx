bca33a259d5053b6ee33178b17184370b4d4cde9
alain-kubwayo <ahkubwayo@gmail.com>
2023-01-06 16:09:44 +0200

    resolve eslint errors

--- components/sections/Hero.jsx
+++ components/sections/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
                     </div>
                     <div className="space-y-6 max-w-[550.8px]">
                         <h2 className="text-[44px] font-bold leading-[2.85rem]">Tech highlights from 2022—in eight charts</h2>
-                        <p className="text-[18px]">Here's a look back at some of the important technology story lines from the year that was.</p>
+                        <p className="text-[18px]">Here&aposs a look back at some of the important technology story lines from the year that was.</p>
                         <Link href="#" className="text-base font-bold flex gap-x-2 group">
                             <span className="group-hover:text-secondary-100">Take a look at tech</span>
                             <Image src="/images/arrow-right.svg" width={24} height={24} alt="Arrow Right" className="group-hover:translate-x-[40%] duration-300" />
